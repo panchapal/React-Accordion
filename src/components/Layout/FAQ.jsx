d4bc84fb9601047015fa407e71bcf8e5f3e0538c
Panchanan Pal <you@wxample.com>
2024-09-16 19:28:09 +0530

    Add new file

---
+++ src/components/Layout/FAQ.jsx
@@ -0,0 +1,15 @@
+export const FAQ = ({ curData, isActive, onToggle }) => {
+    const { question, answer } = curData;
+  
+    return (
+      <li>
+        <div className=" accordion-grid  ">
+          <p className="accordion-question">{question}</p>
+          <button onClick={onToggle} className={isActive ? "active-btn" : ""}>
+            {isActive ? "close" : "show"}{" "}
+          </button>
+        </div>
+        {isActive && <p> {answer} </p>}
+      </li>
+    );
+  };
